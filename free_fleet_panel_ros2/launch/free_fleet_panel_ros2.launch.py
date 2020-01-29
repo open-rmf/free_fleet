ccdd7a8094ec4127a3df8c9f61e98724d862fefe
Aaron <aaronchongth@gmail.com>
2020-01-29 18:00:47 +0800

    launching map server, just worked out the lifecycle_bringup thingy, map_server doesn't seem to be transient local too

---
+++ free_fleet_panel_ros2/launch/free_fleet_panel_ros2.launch.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+
+# Software License Agreement (Apache License)
+#
+# Copyright 2019 Open Source Robotics Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    map_yaml_file_path = LaunchConfiguration(
+        'map_param_file',
+        default=os.path.join(
+            get_package_share_directory('free_fleet_panel_ros2'),
+            'launch',
+            'test_maps',
+            'house.yaml'))
+
+    map_yaml_file_param = {
+        'yaml_filename': map_yaml_file_path}
+    
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
+    
+    return LaunchDescription([
+
+        Node(
+            package='nav2_map_server',
+            node_executable='map_server',
+            node_name='map_server',
+            output='screen',
+            parameters=[map_yaml_file_param],
+            remappings=remappings)
+
+    ])
