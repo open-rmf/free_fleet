7c888ce7a34083a56946edb3c9204d19758d94d7
Aaron Chong <aaronchongth@gmail.com>
2021-01-05 15:56:27 +0800

    Tracking working, tests passing
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- free_fleet/test/unit/test_RobotInfo.cpp
+++ free_fleet/test/unit/test_RobotInfo.cpp
@@ -40,6 +40,26 @@
 #include "src/requests/NavigationRequestInfo.hpp"
 #include "src/requests/RelocalizationRequestInfo.hpp"
 
+void update_and_check_tracking_state(
+  const free_fleet::agv::RobotInfo::SharedPtr& robot_info,
+  const free_fleet::messages::RobotState& new_state,
+  free_fleet::agv::RobotInfo::TrackingState desired_tracking_state,
+  std::size_t desired_tracking_index)
+{
+  rmf_traffic::Time update_time = std::chrono::steady_clock::now();
+  robot_info->update_state(new_state, update_time);
+  CHECK(robot_info->state() == new_state);
+  CHECK(robot_info->last_updated() == update_time);
+
+  auto tracking_estimation = robot_info->tracking_estimation();
+  CHECK(tracking_estimation.first == desired_tracking_state);
+  
+  if (desired_tracking_state != free_fleet::agv::RobotInfo::TrackingState::Lost)
+  {
+    CHECK(tracking_estimation.second == desired_tracking_index);
+  }
+}
+
 SCENARIO("Tests RobotInfo API")
 {
   const std::string test_map_name = "test_level";
@@ -390,16 +410,19 @@ SCENARIO("Tests RobotInfo API")
     auto second_wp = wp;
     second_wp.index = 1;
     second_wp.location.x = 10.0;
+    second_wp.location.y = 0.0;
     nav_request.path.push_back(second_wp);
 
     auto third_wp = wp;
     third_wp.index = 0;
     third_wp.location.x = 0.0;
+    third_wp.location.y = 0.0;
     nav_request.path.push_back(third_wp);
 
     auto forth_wp = wp;
-    forth_wp.index = 2;
-    forth_wp.location.x = -10.0;
+    forth_wp.index = 3;
+    forth_wp.location.x = 0.0;
+    forth_wp.location.y = 10.0;
     nav_request.path.push_back(forth_wp);
 
     bool request_sent = false;
@@ -420,13 +443,179 @@ SCENARIO("Tests RobotInfo API")
     second_state.task_id = new_task_id;
     second_state.location.x = 0.0;
     second_state.location.y = 0.0;
-    rmf_traffic::Time second_time = std::chrono::steady_clock::now();
-    robot_info->update_state(second_state, second_time);
+    update_and_check_tracking_state(
+      robot_info,
+      second_state,
+      TrackingState::OnWaypoint,
+      0);
 
-    // CHECK(robot_info->state() == second_state);
-    // CHECK(robot_info->last_updated() == second_time);
+    // Third state, on the lane
+    auto third_state = initial_state;
+    third_state.task_id = new_task_id;
+    third_state.location.x = 0.0 + 0.5 + 1e-3;
+    third_state.location.y = 0.0;
+    third_state.path_target_index = 1;
+    update_and_check_tracking_state(
+      robot_info,
+      third_state,
+      TrackingState::OnLane,
+      0);
 
-    // auto second_tracking_estimation = robot_info->tracking_estimation();
-    // CHECK(second_tracking_estimation.first == TrackingState::Lost);
+    // Forth state, on the lane
+    auto forth_state = initial_state;
+    forth_state.task_id = new_task_id;
+    forth_state.location.x = 10.0 - 0.5 - 1e-3;
+    forth_state.location.y = 0.0;
+    forth_state.path_target_index = 1;
+    update_and_check_tracking_state(
+      robot_info,
+      forth_state,
+      TrackingState::OnLane,
+      0);
+
+    // Fifth state, on waypoint
+    auto fifth_state = initial_state;
+    fifth_state.task_id = new_task_id;
+    fifth_state.location.x = 10.0 - 0.5 + 1e-3;
+    fifth_state.location.y = 1e-3;
+    fifth_state.path_target_index = 1;
+    update_and_check_tracking_state(
+      robot_info,
+      fifth_state,
+      TrackingState::OnWaypoint,
+      1);
+
+    // Sixth state, on waypoint
+    auto sixth_state = initial_state;
+    sixth_state.task_id = new_task_id;
+    sixth_state.location.x = 10.0;
+    sixth_state.location.y = 0.5 - 1e-3;
+    sixth_state.path_target_index = 2;
+    update_and_check_tracking_state(
+      robot_info,
+      sixth_state,
+      TrackingState::OnWaypoint,
+      1);
+
+    // Seventh state, on lane, a bit lost near waypoint, but with enough
+    // information to get the lane
+    auto seventh_state = initial_state;
+    seventh_state.task_id = new_task_id;
+    seventh_state.location.x = 10.0;
+    seventh_state.location.y = 0.5 + 1e-3;
+    seventh_state.path_target_index = 2;
+    update_and_check_tracking_state(
+      robot_info,
+      seventh_state,
+      TrackingState::OnLane,
+      1);
+    
+    // Eigth state, on lane, on previous waypoint
+    auto eighth_state = initial_state;
+    eighth_state.task_id = new_task_id;
+    eighth_state.location.x = 10.0;
+    eighth_state.location.y = 0.5 - 1e-3;
+    eighth_state.path_target_index = 2;
+    update_and_check_tracking_state(
+      robot_info,
+      eighth_state,
+      TrackingState::OnWaypoint,
+      1);
+
+    // Ninth state, on lane
+    auto ninth_state = initial_state;
+    ninth_state.task_id = new_task_id;
+    ninth_state.location.x = 10.0 - 0.5 - 1e-3;
+    ninth_state.location.y = 0.0;
+    ninth_state.path_target_index = 2;
+    update_and_check_tracking_state(
+      robot_info,
+      ninth_state,
+      TrackingState::OnLane,
+      1);
+
+    // Tenth state, on waypoint
+    auto tenth_state = initial_state;
+    tenth_state.task_id = new_task_id;
+    tenth_state.location.x = 0.5 - 1e-3;
+    tenth_state.location.y = 0.0;
+    tenth_state.path_target_index = 3;
+    update_and_check_tracking_state(
+      robot_info,
+      tenth_state,
+      TrackingState::OnWaypoint,
+      0);
+    
+    // Eleventh state, on lane
+    auto eleventh_state = initial_state;
+    eleventh_state.task_id = new_task_id;
+    eleventh_state.location.x = 0.0;
+    eleventh_state.location.y = 0.5 + 1e-3;
+    eleventh_state.path_target_index = 3;
+    update_and_check_tracking_state(
+      robot_info,
+      eleventh_state,
+      TrackingState::OnLane,
+      4);
+
+    // Twelth state, on waypoint
+    auto twelth_state = initial_state;
+    twelth_state.task_id = new_task_id;
+    twelth_state.location.x = 0.0;
+    twelth_state.location.y = 10.0 - 0.5 + 1e-3;
+    twelth_state.path_target_index = 3;
+    update_and_check_tracking_state(
+      robot_info,
+      twelth_state,
+      TrackingState::OnWaypoint,
+      3);
+
+    // Thirteenth state, on waypoint
+    auto thirteenth_state = initial_state;
+    thirteenth_state.task_id = new_task_id;
+    thirteenth_state.location.x = 0.0;
+    thirteenth_state.location.y = 10.0;
+    thirteenth_state.path_target_index = 3;
+    update_and_check_tracking_state(
+      robot_info,
+      thirteenth_state,
+      TrackingState::OnWaypoint,
+      3);
+
+    // Forteenth state, task done, on waypoint
+    auto forteenth_state = initial_state;
+    forteenth_state.task_id = 0;
+    forteenth_state.location.x = 0.0;
+    forteenth_state.location.y = 10.0;
+    forteenth_state.path_target_index = 0;
+    update_and_check_tracking_state(
+      robot_info,
+      forteenth_state,
+      TrackingState::OnWaypoint,
+      3);
+
+    // Fifteenth state, task done, lost
+    auto fifteenth_state = initial_state;
+    fifteenth_state.task_id = 0;
+    fifteenth_state.location.x = -0.5 - 1e-3;
+    fifteenth_state.location.y = 10.0 + 0.5 + 1e-3;
+    fifteenth_state.path_target_index = 0;
+    update_and_check_tracking_state(
+      robot_info,
+      fifteenth_state,
+      TrackingState::Lost,
+      0);
+
+    // Sixteenth state, task done, back on waypoint
+    auto sixteenth_state = initial_state;
+    sixteenth_state.task_id = 0;
+    sixteenth_state.location.x = -0.5 + 1e-3;
+    sixteenth_state.location.y = 10.0;
+    sixteenth_state.path_target_index = 0;
+    update_and_check_tracking_state(
+      robot_info,
+      sixteenth_state,
+      TrackingState::OnWaypoint,
+      3);
   }
 }
