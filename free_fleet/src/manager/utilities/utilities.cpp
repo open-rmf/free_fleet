aec68b9c05b968b9302de95b62481435644058ff
Aaron Chong <aaronchongth@gmail.com>
2021-07-02 19:57:32 +0800

    Changed distance_to_lane implementation
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- free_fleet/src/manager/utilities/utilities.cpp
+++ free_fleet/src/manager/utilities/utilities.cpp
@@ -35,20 +35,17 @@ double distance_to_lane(
   const rmf_traffic::agv::Graph& graph,
   const Eigen::Vector2d& coordinates)
 {
-  const Eigen::Vector2d entry_loc =
+  const Eigen::Vector2d p0 =
     graph.get_waypoint(lane.entry().waypoint_index()).get_location();
-  const Eigen::Vector2d exit_loc =
+  const Eigen::Vector2d p1 =
     graph.get_waypoint(lane.exit().waypoint_index()).get_location();
-  const Eigen::Vector3d entry_p = {entry_loc[0], entry_loc[1], 1.0};
-  const Eigen::Vector3d exit_p = {exit_loc[0], exit_loc[1], 1.0};
+  const double lane_length = (p1 - p0).norm();
+  
+  const Eigen::Vector2d pn = (p1 - p0) / lane_length;
+  const Eigen::Vector2d p_l = coordinates - p0;
+  const double p_l_projection = p_l.dot(pn);
 
-  Eigen::Vector3d line_coef = entry_p.cross(exit_p);
-  line_coef = line_coef / line_coef[2];
-
-  const double dist =
-    abs(line_coef[0] * coordinates[0] + line_coef[1] * coordinates[1] + 
-    line_coef[2]) / sqrt(pow(line_coef[0], 2.0) + pow(line_coef[1], 2.0));
-  return dist;
+  return (p_l - p_l_projection*pn).norm();
 }
 
 //==============================================================================
