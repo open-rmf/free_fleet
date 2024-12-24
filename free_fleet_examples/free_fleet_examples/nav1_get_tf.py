0e9d183ce290f267952e8abf148053d52134c904
Aaron Chong <aaronchongth@gmail.com>
2024-12-24 15:03:19 +0800

    Renaming and verifying regression tests
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- free_fleet_examples/free_fleet_examples/nav1_get_tf.py
+++ free_fleet_examples/free_fleet_examples/nav1_get_tf.py
@@ -14,45 +14,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# https://github.com/eclipse-zenoh/zenoh-plugin-ros1/issues/131
+# # https://github.com/eclipse-zenoh/zenoh-plugin-ros1/issues/131
 
-import zenoh, time
-from rosbags.typesys import Stores, get_types_from_msg, get_typestore
+# import zenoh, time
+# from rosbags.typesys import Stores, get_types_from_msg, get_typestore
 
-TOPIC = '/tf'
-# TYPE = 'std_msgs/msg/String'
-TYPE = 'tf2_msgs/msg/TFMessage'
+# TOPIC = '/tf'
+# # TYPE = 'std_msgs/msg/String'
+# TYPE = 'tf2_msgs/msg/TFMessage'
 
-# Noetic TFMessage message definition, taken from:
+# # Noetic TFMessage message definition, taken from:
+# #
 # https://github.com/ros/geometry2/blob/noetic-devel/tf2_msgs/msg/TFMessage.msg
-TFMESSAGE_DEFINITION = """
-geometry_msgs/TransformStamped[] transforms
-"""
+# TFMESSAGE_DEFINITION = """
+# geometry_msgs/TransformStamped[] transforms
+# """
 
-def get_zenoh_name_of_ros1_topic(ros1_store, topic: str, msg_type: str) -> str:
-    # Get md5 and encode msg_type to construct zenoh topic
-    msg_type_split = msg_type.split('/')
-    msg_type_encoded = '/'.join([msg_type_split[0],msg_type_split[2]]).encode('utf-8').hex()
-    md5 = ros1_store.generate_msgdef(msg_type)[1]
-    zenoh_topic = '/'.join([msg_type_encoded, md5, topic[1:]])
+# def get_zenoh_name_of_ros1_topic(ros1_store, topic: str, msg_type: str
+# ) -> str:
+#     # Get md5 and encode msg_type to construct zenoh topic
+#     msg_type_split = msg_type.split('/')
+#     msg_type_encoded = \
+#         '/'.join([msg_type_split[0],msg_type_split[2]]).encode('utf-8').hex()
+#     md5 = ros1_store.generate_msgdef(msg_type)[1]
+#     zenoh_topic = '/'.join([msg_type_encoded, md5, topic[1:]])
 
-    return zenoh_topic
-
-
-def listener(sample: zenoh.Sample):
-    msg = ros1_store.deserialize_ros1(sample.payload.to_bytes(), TYPE)
-    print(f'ROS1 msg: {msg}')
+#     return zenoh_topic
 
 
-if __name__ == "__main__":
-    session = zenoh.open(zenoh.Config())
-    ros1_store = get_typestore(Stores.ROS1_NOETIC)
+# def listener(sample: zenoh.Sample):
+#     msg = ros1_store.deserialize_ros1(sample.payload.to_bytes(), TYPE)
+#     print(f'ROS1 msg: {msg}')
 
-    ros1_store.register(
-        get_types_from_msg(TFMESSAGE_DEFINITION, TYPE),
-    )
 
-    zenoh_topic = get_zenoh_name_of_ros1_topic(ros1_store, topic=TOPIC, msg_type=TYPE)
-    print(f'ROS topic {TOPIC} is converted to Zenoh {zenoh_topic}')
-    sub = session.declare_subscriber(zenoh_topic, listener)
-    time.sleep(60)
+# if __name__ == "__main__":
+#     session = zenoh.open(zenoh.Config())
+#     ros1_store = get_typestore(Stores.ROS1_NOETIC)
+
+#     ros1_store.register(
+#         get_types_from_msg(TFMESSAGE_DEFINITION, TYPE),
+#     )
+
+#     zenoh_topic = get_zenoh_name_of_ros1_topic(
+#         ros1_store,
+#         topic=TOPIC,
+#         msg_type=TYPE
+#     )
+#     print(f'ROS topic {TOPIC} is converted to Zenoh {zenoh_topic}')
+#     sub = session.declare_subscriber(zenoh_topic, listener)
+#     time.sleep(60)
--- free_fleet_examples/free_fleet_examples/nav1_send_move_base_simple_goal.py
+++
@@ -1,2 +0,0 @@
-# rostopic pub /move_base_simple/goal geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 1.0, y: 0.0, z: 0.0}, orientation: {w: 1.0}}}'
-# rostopic echo /move_base/status
